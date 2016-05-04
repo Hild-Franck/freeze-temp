ffbca205f0032b7736336c5f9fbd819cc35dc8ad
Vallab <valentinlablanche@orange.fr>
2016-05-04 11:19:51 +0200

    Début dev sideMenu

--- app/views/sideMenu/sideMenu.js
+++ app/views/sideMenu/sideMenu.js
@@ -1,3 +1,10 @@
 /**
  * Created by Knarfux on 04/05/2016.
  */
+angular.module('includeExample', ['ngAnimate'])
+    .controller('ExampleController', ['$scope', function($scope) {
+        $scope.templates =
+            [ { name: 'sideMenu.html', url: 'views/sideMenu/sideMenu.html'}
+               ];
+        $scope.template = $scope.templates[0];
+    }]);
